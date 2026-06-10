ab6276042978c79cb1a7fceb99a3f275a914eb78
Betsy <betsygeorge1124@gmail.com.com>
2026-06-10 16:51:55 +0530

    Update students URL configuration

--- backend/students/urls.py
+++ backend/students/urls.py
@@ -1,7 +1,12 @@
+from django.http import JsonResponse
 from django.urls import path
 from .views import login_view, student_list_create_view, student_retrieve_update_delete_view
 
+def api_home(request):
+    return JsonResponse({"message": "API is working"})
+
 urlpatterns = [
+     path('', api_home),
     path('login/', login_view, name='login'),
     path('students/', student_list_create_view, name='students-list-create'),
     path('students/<int:pk>/', student_retrieve_update_delete_view, name='student-detail'),
