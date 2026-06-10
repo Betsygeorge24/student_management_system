d6cf93b3449f9d42581e2f251897641f5243643b
Betsy <betsygeorge1124@gmail.com.com>
2026-06-10 18:29:46 +0530

    commit and push

--- backend/students/migrations/0002_create_default_data.py
+++ backend/students/migrations/0002_create_default_data.py
@@ -1,23 +1,25 @@
 from django.db import migrations
-
+from django.contrib.auth.hashers import make_password
 
 def create_default_data(apps, schema_editor):
     Student = apps.get_model('students', 'Student')
     User = apps.get_model('auth', 'User')
 
     user, created = User.objects.get_or_create(
-    username='admin',
-    defaults={
-        'email': 'admin@example.com',
-        'is_staff': True,
-        'is_superuser': True,
-    }
-)
+        username='admin',
+        defaults={
+            'email': 'admin@example.com',
+            'is_staff': True,
+            'is_superuser': True,
+            'password': make_password('adminpass'),  
+        }
+    )
 
-    user.set_password('adminpass')
-    user.is_staff = True
-    user.is_superuser = True
-    user.save()
+    if not created:
+        user.password = make_password('adminpass')
+        user.is_staff = True
+        user.is_superuser = True
+        user.save()
 
     if not Student.objects.exists():
         Student.objects.create(name='Maria Rivera', email='maria.rivera@example.com', age=15, grade='10')
