256a3198a418056f6425282e430d3ce29163d74e
Betsy <betsygeorge1124@gmail.com.com>
2026-06-10 16:27:02 +0530

    Commit backend urls change

--- backend/school_management/urls.py
+++ backend/school_management/urls.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
+from django.http import HttpResponse
 from django.urls import path, include
+def home(request):
+    return HttpResponse("Backend is working successfully 🚀")
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
