41cda431858996d64132f1f7d8a40954cf225e90
Betsy <betsygeorge1124@gmail.com.com>
2026-06-10 17:46:48 +0530

    fix cors and csrf for vercel mobile issue

--- backend/school_management/settings.py
+++ backend/school_management/settings.py
@@ -6,8 +6,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('DJANGO_SECRET_KEY', 'django-insecure-please-change-this-secret-key')
 DEBUG = os.getenv('DJANGO_DEBUG', 'True').lower() in ('true', '1')
 ALLOWED_HOSTS = ['*']
-
-CSRF_TRUSTED_ORIGINS = ['https://*.onrender.com']
+CSRF_TRUSTED_ORIGINS = [
+    "https://student-management-system-sandy-omega.vercel.app",
+    "https://*.onrender.com"
+]
+CORS_ALLOWED_ORIGINS = [
+    "https://student-management-system-sandy-omega.vercel.app",
+]
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
